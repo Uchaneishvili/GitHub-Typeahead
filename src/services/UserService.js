9c5f0a7c27eb8fa666881015753cf6b26b3289ff
Giga <g.uchaneishvili>
2024-01-19 00:06:53 +0100

    credentials fixed

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,4 @@
-const GITHUB_ACCESS_TOKEN = `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`
+const GITHUB_ACCESS_TOKEN = `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`
 
 export const getUsers = async (query) => {
   return await fetch(`https://api.github.com/search/users?q=${query}`, {
