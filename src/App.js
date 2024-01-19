3f0ed78100d665036d71ded194ebd20e3362d9fe
Giga <g.uchaneishvili>
2024-01-19 23:46:28 +0100

    Footer is added, responsive is fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css'
 import Search from './components/Search/Search'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <div>
       <Header />
-      <Search />
+      <main>
+        <Search />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './Header.module.css'
+import { GitHub } from '../../icons/github'
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1>Github Typeahead</h1>
+      <div className={styles.innerContainer}>
+        <div className={styles.logoContainer}>
+          <GitHub />
+        </div>
+        <div className={styles.titleContainer}>
+          <span>Github Typeahead</span>
+        </div>
+      </div>
     </div>
   )
 }
