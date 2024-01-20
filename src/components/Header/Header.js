5ab78a56190ab3efcfb11eb7ac61b171dd214a14
Giga <g.uchaneishvili>
2024-01-20 03:25:28 +0100

    header logo is changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './Header.module.css'
-import { GitHub } from '../../icons/github'
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.innerContainer}>
         <div className={styles.logoContainer}>
-          <GitHub />
+          <img
+            src='./assets/GitHub_Logo_Animation.gif'
+            alt='GitHub'
+            width={40}
+          />
         </div>
         <div className={styles.titleContainer}>
           <span>Github Typeahead</span>
